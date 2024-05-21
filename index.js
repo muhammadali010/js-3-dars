eaaa85186375576340603d3d4509e17e0bdc6617
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-05-21 14:41:52 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,136 @@
+// 1chi masala
+// let a = + prompt("a ni kiriting");
+// if (a == 0) {
+//     console.log(a + ' soni nitral');
+// }
+// if (a > 0) {
+//     console.log(a + ' soni musbat');
+// } else {
+//     console.log(a + ' soni manfiy');
+// }
+
+
+// // 2chi masala
+// let a =+ prompt("a sonini kiriting");
+// if (a % 2 == 1){
+// console.log( a + ' soni toq');
+// } else {
+// console.log( a + ' soni juft');
+// }
+
+
+// 3chi masala
+// let a =+ prompt("a sonini kiriting");
+// if (a % 2 == 0){
+// console.log( a + ' soni juft');
+// } else {
+// console.log( a + ' soni toq');
+// }
+
+
+// 4chi masala
+// let a =+ prompt("birinchi sonni kiriting");
+// let b =+ prompt("ikkinchi sonni kiriting");
+// if (a >2 && b <=3){
+// console.log("true");
+// } else {
+// console.log("false");
+// }
+
+
+// 5chi masala
+// let a =+ prompt("birinchi sonni kiriting");
+// let b =+ prompt("ikkinchi sonni kiriting");
+// if (a>=0 || b <-2){
+// console.log("true");
+// } else {
+// console.log("false");
+// }
+
+
+// 6chi masala
+// let a =+ prompt("birinchi sonni kiriting");
+// let b =+ prompt("ikkinchi sonni kiriting");
+// let c =+ prompt("uchinchi sonni kiriting")
+// if (a<=b<=c){
+// console.log("true");
+// } else {
+// console.log("false");
+// }
+
+
+// 7chi masala
+// let a =+prompt("a ni kiriting");
+// let b =+prompt("b ni kiriting");
+// let c =+prompt("c ni kiriting");
+// if (a<b<c){
+// console.log('true');
+// }else {
+// console.log('false');
+// }
+
+
+// 8chi masala
+// let a =+prompt("a ni kiriting");
+// let b =+prompt("b ni kiriting");
+// if (a && b % 2 == 1){
+// console.log('a va B soni toq');
+// } else {
+// console.log( 'a va b soni toq emas');
+// }
+
+
+// 9chi masala
+// let a =+prompt("a ni kiriting");
+// let b =+prompt("b ni kiriting");
+// if (a || b % 2 == 1){
+// console.log('a va B sonini hech bolmaganda 1 tasi toq');
+// } else {
+// console.log( 'a va b sonidan 1tasi ham toq emas');
+// }
+
+
+// 10chi masala
+// let a = + prompt('a ni kiriting');
+// let b = + prompt('b ni kiriting');
+// if (a && b % 2 == 0) {
+//     console.log('ikkalasiham toq emas');
+// }
+// if (a || b % 2 == 1) {
+//     console.log('a va b sonini faqat 1 tasi toq');
+// }else if (a && b % 2 == 1) {
+//     console.log('a va b sonlari toq');
+// }
+
+
+// 11chi masala
+// let a = + prompt('a ni kiriting');
+// let b = + prompt('b ni kiriting');
+// if (a && b % 2 == 0 ){
+// console.log("ikkalasiham juft");
+// }
+// if (a && b % 2 == 1){
+// console.log('ikkalasiham toq');
+// }
+
+
+// 12chi masala
+// let a =+prompt('a ni kiriting');
+// let b =+prompt('b ni kiriting');
+// let c =+prompt('c ni kiriting');
+// if (a && b && c > 0){
+// console.log("true");
+// } else{
+// console.log("false");
+// }
+
+
+// 13chi masala
+// let a =+prompt('a ni kiriting');
+// let b =+prompt('b ni kiriting');
+// let c =+prompt('c ni kiriting');
+// if (a || b || c > 0){
+// console.log("true");
+// } else{
+// console.log("false");
+// }
